f1d194cf3339a2e8ba3d9487f5edb7bc7dd7469f
Steven Truesdell <steven@strues.io>
2016-05-09 13:46:56 -0600

    barebones to the T express server. Added mocha, sinon, chai for testing.

---
+++ src/server/app.js
@@ -0,0 +1,51 @@
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+import Express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import methodOverride from 'method-override';
+import morgan from 'morgan';
+
+const app = Express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride());
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Running'); // eslint-disable-line
+});
+
+export default app;
