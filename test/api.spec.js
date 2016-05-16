75d1caaad3311800755ba1dcd16381716570de13
Steven Truesdell <steven@strues.io>
2016-05-16 09:21:08 -0600

    update new test

--- test/api.spec.js
+++ test/api.spec.js
@@ -18,4 +18,22 @@ describe('API tests', () => {
         });
     });
   });
+  describe('Get all reports', () => {
+    it('returns 200 on /reports', done => {
+      request(app)
+        .get('/api/v1/reports')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .expect(res => {
+          res.body.DoH = '2/18/2013';
+          res.body.ITS_016_001 = '1/27/2016';
+        })
+        .end(err => {
+          if (err) {
+            throw new Error(err);
+          }
+          done();
+        });
+    });
+  });
 });
